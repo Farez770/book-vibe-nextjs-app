e13612b736f0d5c259cf059171c47769a91bc7be
Farez770 <asfaq.farez@gmail.com>
2026-09-24 23:30:48 +0600

    Edit some changes and add .env

--- src/context/BooksContextProvider.tsx
+++ src/context/BooksContextProvider.tsx
@@ -1,7 +1,22 @@
 "use client";
+// import { IBook } from "@/types/books.type";
 import React, { createContext, ReactNode, useState } from "react";
 
+// interface IBooksContext {
+//   readBooks: IBook[];
+//   setReadBooks: React.Dispatch<React.SetStateAction<IBook[]>>;
+//   wishlist: IBook[];
+//   setWishlist: React.Dispatch<React.SetStateAction<IBook[]>>;
+// }
+
 export const BooksContext = createContext({});
+// export const BooksContext = createContext<IBooksContext>({});
+// export const BooksContext = createContext<IBooksContext>({
+//   readBooks: [],
+//   setReadBooks: () => {},
+//   wishlist: [],
+//   setWishlist: () => {},
+// });
 
 const BooksContextProvider = ({ children }: { children: ReactNode }) => {
   const [readBooks, setReadBooks] = useState([]);
