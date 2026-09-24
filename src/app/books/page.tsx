8f097342da10f71c491a111b337a1eb4f200c8d4
Farez770 <asfaq.farez@gmail.com>
2026-09-24 23:18:45 +0600

    do some changes on data fetching

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -6,9 +6,16 @@ import BooksCard from "@/components/shared/BooksCard";
 // type Props = {};
 
 const getBooks = async () => {
-  const res = await fetch("http://localhost:3000//booksData.json");
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching books data:", error);
+    return [];
+  }
 };
 
 const page = async () => {
