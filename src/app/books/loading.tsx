a7ed660d0b92c80d7f956f628100c04d71bc0209
Farez770 <asfaq.farez@gmail.com>
2026-09-24 22:34:29 +0600

    Implement Sort By Functionality on listed book.

---
+++ src/app/books/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BooksLoading = () => {
+  return <div>Book Data is Loading....</div>;
+};
+
+export default BooksLoading;
--- src/app/listed-books/page.tsx
+++ src/app/listed-books/page.tsx
@@ -3,23 +3,56 @@
 import ListedBooksCard from "@/components/shared/ListedBooksCard";
 import { BooksContext } from "@/context/BooksContextProvider";
 import { IBook } from "@/types/books.type";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 // type Props = {}
 
 const ListedBooks = () => {
+  const [sortBy, setSortBy] = useState<"rating" | "pages" | "year">("rating");
   const { readBooks, wishlist } = useContext(BooksContext) as {
     readBooks: IBook[];
     wishlist: IBook[];
   };
 
-  console.log(readBooks, wishlist, "Read bOkks", "WishList");
+  // console.log(readBooks, wishlist, "Read bOkks", "WishList");
+
+  const sortBooks = (books: IBook[]) => {
+    const sortedBooks = [...books];
+    if (sortBy === "rating") {
+      sortedBooks.sort((a, b) => b.rating - a.rating);
+    } else if (sortBy === "pages") {
+      sortedBooks.sort((a, b) => b.totalPages - a.totalPages);
+    } else if (sortBy === "year") {
+      sortedBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+    }
+
+    return sortedBooks;
+  };
+
+  const sortedReadBooks = sortBooks(readBooks);
+  const sortedWishlist = sortBooks(wishlist);
 
   return (
     <div className="container mx-auto px-6 py-[20px]">
-      <h1 className="my-4 bg-amber-100 rounded-3xl py-8 font-bold text-4xl text-center">
+      <h1 className="my-4 bg-gray-200 rounded-2xl py-8 font-bold text-4xl text-center">
         Listed Books
       </h1>
+
+      <div className="text-center my-4">
+        <select
+          value={sortBy}
+          onChange={(e) =>
+            setSortBy(e.target.value as "rating" | "pages" | "year")
+          }
+          defaultValue="Sort By"
+          className="select select-success "
+        >
+          <option disabled={true}>Sort By</option>
+          <option value={"rating"}>Rating</option>
+          <option value={"pages"}>Number of pages</option>
+          <option value={"year"}>Publisher year</option>
+        </select>
+      </div>
       {/* <h1>
         Total Read Book : {readBooks.length} | Total wishlist book :{" "}
         {wishlist.length}
@@ -34,8 +67,8 @@ const ListedBooks = () => {
             aria-label={`Read Books (${readBooks.length})`}
           />
           <div className="tab-content bg-base-100 border-base-300 p-6 space-y-4">
-            {readBooks.length > 0 ? (
-              readBooks.map((book: IBook) => {
+            {sortedReadBooks.length > 0 ? (
+              sortedReadBooks.map((book: IBook) => {
                 return <ListedBooksCard key={book.bookId} book={book} />;
               })
             ) : (
@@ -53,8 +86,8 @@ const ListedBooks = () => {
             defaultChecked
           />
           <div className="tab-content bg-base-100 border-base-300 p-6 space-y-4">
-            {wishlist.length > 0 ? (
-              wishlist.map((book: IBook) => {
+            {sortedWishlist.length > 0 ? (
+              sortedWishlist.map((book: IBook) => {
                 return <ListedBooksCard key={book.bookId} book={book} />;
               })
             ) : (
